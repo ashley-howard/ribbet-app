1be01d527e9b88e674ab89cb828e6baef7f94428
noteconvert <me@ashley.how>
2020-02-01 14:44:32 +0000

    add pomodoro timer

--- scripts/index.js
+++ scripts/index.js
@@ -82,9 +82,9 @@ function saveSettings() {
     changeScreen('add');
     console.log('Dev Mode Off');
   }
-  if (darkMode.checked === true){
+  if (darkMode.checked === true) {
   }
-  else{
+  else {
   }
 }
 
@@ -335,6 +335,7 @@ function changeScreen(div) {
     orderScreen.style.display = "none";
     doScreen.style.display = "none";
     settingsPage.style.display = "block";
+    document.getElementById('navbarNav').className = 'navbar-collapse collapse'
   }
 }
 
@@ -351,4 +352,136 @@ else {
 function clearStorage() {
   localStorage.clear();
   document.getElementById('clearText').innerHTML = 'Please refresh the page to see changes';
+}
+
+const pomodoroContainer = document.getElementById('pomodoro-container')
+const pomodoroLink = document.getElementById('pomodoro-link')
+const pomodoroTime = document.getElementById("pomodoro-input");
+const shortBreakTime = document.getElementById("short-break-input");
+const longBreakTime = document.getElementById("long-break-input");
+const countdown = document.getElementById("countdown");
+
+countdown.innerText = `${pomodoroTime.value}:00`;
+
+var counter = 0;
+var minutes = 0;
+var seconds = 0;
+var interval;
+var paused;
+
+function pomodoro() {
+  if (counter < 3) {
+    if (paused === true) {
+      console.log("timer unpaused")
+      paused = false;
+    }
+    else {
+      counter += 1;
+      console.log(counter)
+      console.log("pomodoro")
+      seconds = pomodoroTime.value * 60 || 0;
+    }
+
+    interval = setInterval(function () {
+      seconds--;
+      countdown.innerText = `${(minutes = Math.floor(seconds / 60))}:${seconds -
+        minutes * 60}`;
+      if (!seconds) {
+        clearInterval(interval);
+        shortBreak()
+      }
+    }, 1000);
+  }
+
+  else {
+    counter += 1;
+    console.log(counter)
+    console.log("pomodoro")
+    seconds = pomodoroTime.value * 60 || 0;
+    interval = setInterval(function () {
+      seconds--;
+      countdown.innerText = `${(minutes = Math.floor(seconds / 60))}:${seconds -
+        minutes * 60}`;
+      if (!seconds) {
+        clearInterval(interval);
+        longBreak()
+      }
+    }, 1000);
+  }
+}
+
+function shortBreak() {
+  console.log("short break")
+  seconds = shortBreakTime.value * 60 || 0;
+  interval = setInterval(function () {
+    seconds--;
+    countdown.innerText = `${(minutes = Math.floor(seconds / 60))}:${seconds -
+      minutes * 60}`;
+    if (!seconds) {
+      clearInterval(interval);
+      pomodoro();
+    }
+  }, 1000);
+}
+
+function longBreak() {
+  console.log("long break")
+  seconds = longBreakTime.value * 60 || 0;
+  interval = setInterval(function () {
+    seconds--;
+    countdown.innerText = `${(minutes = Math.floor(seconds / 60))}:${seconds -
+      minutes * 60}`;
+    if (!seconds) {
+      clearInterval(interval);
+      counter = 0;
+      pomodoro();
+    }
+  }, 1000);
+}
+
+function pauseTimer() {
+  if (!paused) {
+    console.log('timer paused')
+    paused = true;
+    clearInterval(interval);
+  } else {
+    console.log('already paused!')
+  }
+}
+
+function resetTimer() {
+  console.log('timer reset')
+  clearInterval(interval);
+  updateSettings();
+  paused = false;
+  counter = 0;
+}
+
+function updateSettings() {
+  if (!Number.isInteger(+pomodoroTime.value) || !Number.isInteger(+shortBreakTime.value) || !Number.isInteger(+longBreakTime.value)) {
+    window.alert("please enter an integer")
+  } else {
+    countdown.innerText = `${pomodoroTime.value}:00`;
+  }
+  counter = 0;
+  clearInterval(interval);
+}
+
+function resetSettings() {
+  pomodoroTime.value = 25;
+  shortBreakTime.value = 5;
+  longBreakTime.value = 10;
+  counter = 0;
+  resetTimer();
+}
+
+function openPomodoro() {
+  if (pomodoroContainer.style.display === "block") {
+    pomodoroContainer.style.display = "none";
+    pomodoroLink.innerHTML = 'Pomodoro: Off';
+  } else {
+    pomodoroContainer.style.display = "block";
+    pomodoroLink.innerHTML = 'Pomodoro: On';
+  }
+  document.getElementById('navbarNav').className = 'navbar-collapse collapse'
 }
